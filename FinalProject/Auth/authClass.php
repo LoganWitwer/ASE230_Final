bd6286e594018880d8b0dd8802fb9d224fe1b7c3
LoganWitwer <113311699+LoganWitwer@users.noreply.github.com>
2022-12-03 21:10:50 -0500

    Add files via upload

--- FinalProject/Auth/authClass.php
+++ FinalProject/Auth/authClass.php
@@ -1,11 +1,8 @@
 <?php
-
+require_once('../Settings/settings.php');
 class Auth{
-    public static function signup(){
-        $mysqli = new mysqli("127.0.0.1","root","","tofoo");
-        $fullName = $_POST['fullName'];
-        $email = $_POST['email'];
-        $shippingAddress = $_POST['shippingAddress'];
-        $result = $mysqli->query("INSERT INTO user (name, email, isAdmin) VALUES ('$fullName', '$email', '0')");
+    public static function signup($connection, $name, $email, $password, $isAdmin){
+        $query=$connection->prepare('INSERT INTO user (`name`, `email`, `password`, `isAdmin`) VALUES (?,?,?,?)');
+        $query->execute([$name, $email, $password, $isAdmin]);
     }
 }
--- FinalProject/Auth/signup.php
+++ FinalProject/Auth/signup.php
@@ -8,24 +8,9 @@
 </form><hr />
 
 <?php
-session_start();
-require_once('../Settings/settings.php');
-// require_once('../User/userClass.php');
-if(count($_POST)>0){
-    //User::create();
-    $query=$connection->prepare('INSERT INTO user (name, email, password, isAdmin) VALUES (?,?,?,?)');
-    $query->execute([$_POST['fullName'], $_POST['email'], $_POST['password'], '0']);
-    
-    
-    //$query=$connection->prepare('INSERT INTO user (name, email, password, isAdmin) VALUES (?)');
-    //$query->execute($_POST['fullName'], $_POST['email'], $_POST['password'], '0');
 
-           
+require_once('../Settings/settings.php');
+require_once('../Auth/authClass.php');
+if(count($_POST)>0){
+    AUTH::signup($connection, $_POST['fullName'], $_POST['email'], $_POST['password'], 0);    
 }
-/*function signup(){
-    $mysqli = new mysqli("127.0.0.1","root","","tofoo");
-    $fullName = $_POST['fullName'];
-    $email = $_POST['email'];
-    $shippingAddress = $_POST['shippingAddress'];
-    $result = $mysqli->query("INSERT INTO user (name, email, isAdmin) VALUES ('$fullName', '$email', '0')");
-}*/
