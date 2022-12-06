83e606e83c130a4aab0bfa3405ec725c7f59a031
LoganWitwer <113311699+LoganWitwer@users.noreply.github.com>
2022-12-06 17:20:22 -0500

    Add files via upload

--- FinalProject/Restaurant/restaurantClass.php
+++ FinalProject/Restaurant/restaurantClass.php
@@ -7,4 +7,24 @@ class Restaurant{
         $query=$connection->prepare('INSERT INTO restaurant (`name`, `address`, `category`) VALUES (?,?,?)');
         $query->execute([$name, $address, $category]);
     }
+
+    public static function read($connection){
+       //if SESSION['role']==1; list each item with delete, and modify links that include the restaurant ID
+       $query=$connection->query('SELECT `name`, `restaurantID` FROM restaurant');
+       while($result=$query->fetch()){
+        echo '<table>
+        <tr>
+            <td>'.$result['name'].'</td>
+            <td><a href="delete.php?id='.$result['restaurantID'].'">Delete</a></td>
+            <td><a href="modify.php?id='.$result['restaurantID'].'">Modify</a></td>
+        </tr>
+        </table>';
+    }
+       //else; list each restaurant with order options
+    }
+
+    public static function delete($connection, $delete){
+
+    }
+
 }
