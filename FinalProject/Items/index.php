31065baf9fc5e987f01b7067f687b3c9e6486e2c
LoganWitwer <113311699+LoganWitwer@users.noreply.github.com>
2022-12-07 16:40:00 -0500

    Add files via upload

--- FinalProject/Items/index.php
+++ FinalProject/Items/index.php
@@ -2,7 +2,14 @@
 session_start();
 require_once('../Settings/settings.php');
 require_once('itemClass.php');
-Item::read($connection);
-$restaurant=$_GET['id'];
+
+if($_SESSION['role']==1){
+    Item::read($connection, 1, $_GET['id']);
+    $restaurant=$_GET['id'];
+    echo '<a href="create.php?id='.$restaurant.'">Create Item</a>';
+} else {
+    Item::read($connection, 0, $_GET['id']);
+}
+
+
 ?>
-<a href="create.php?id=<?$restaurant?>">Create Item</a>
--- FinalProject/Items/itemClass.php
+++ FinalProject/Items/itemClass.php
@@ -7,19 +7,28 @@ class Item{
         $query->execute([$description, $price, $itemName, $restaurant]);
     }
 
-    public static function read($connection){
+    public static function read($connection, $role, $restaurantID){
         //if SESSION['role']==1; list each item using the restaurant id and include a delete and modify option that passes the itemID
-        $query=$connection->query('SELECT `itemName`, `description`, `price`, `itemID` FROM items');
-        while($result=$query->fetch()){
-        echo '<table>
-            <tr>
-                <td>'.$result['itemName'].'</td>
-                <td><a href="delete.php?id='.$result['itemID'].'">Delete</a></td>
-                <td><a href="modify.php?id='.$result['itemID'].'">Modify</a></td>
-            </tr>
-            </table>';
-        }
-        //else; list each item with order options
+        if($role==1){
+            $query=$connection->prepare('SELECT `itemName`, `description`, `price`, `itemID` FROM items WHERE `restaurantID`=?');
+            $query->execute([$restaurantID]);
+            while($result=$query->fetch()){
+                echo '<table>
+                    <tr>
+                        <td>'.$result['itemName'].'</td>
+                        <td><a href="delete.php?id='.$result['itemID'].'">Delete</a></td>
+                        <td><a href="modify.php?id='.$result['itemID'].'">Modify</a></td>
+                    </tr>
+                </table>';
+            }
+        } /*else {//else; list each item with order options
+            $query=$connection->prepare('SELECT `itemName`, `description`, `price`, `itemID` FROM items WHERE `restaurantID`=?');
+            $query->execute([$restaurantID]);
+            while($result=$query->fetch()){
+                
+            }
+        }*/
+
         }
 
         public static function delete($connection, $delete){
