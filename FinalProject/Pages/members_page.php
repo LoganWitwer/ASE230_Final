c97ad64c50851adf622bf0e4167f53c592456992
LoganWitwer <113311699+LoganWitwer@users.noreply.github.com>
2022-12-09 15:49:07 -0500

    Add files via upload

--- FinalProject/Pages/members_page.php
+++ FinalProject/Pages/members_page.php
@@ -4,12 +4,15 @@ require_once('../Restaurant/restaurantClass.php');
 $userID = $_SESSION['ID'];
 $userName = $_SESSION['name'];
 if($_SESSION['logged']==false){
-    header('location: vistor_page.php');
+    header('location: visitor_page.php');
 }
 echo 'Hello '.$userName.'!';
 Restaurant::read($connection, 0);
 
 ?>
 <br>
-<a href="../Order/index.php">Place Order</a>
+<a href="../Order/checkout.php">Check out</a>
+<a href="../Deliveryaddress/create.php"> Add Delivery Address </a>
+
+<a href="../PaymentMethod/create.php"> Add Card </a>
 <a href="../Auth/signout.php">Sign out</a>
