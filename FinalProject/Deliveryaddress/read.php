3db474d3a6fe1c40e17d023c08dc0d054c86f181
LoganWitwer <113311699+LoganWitwer@users.noreply.github.com>
2022-12-11 15:53:30 -0500

    Add files via upload

--- FinalProject/Deliveryaddress/read.php
+++ FinalProject/Deliveryaddress/read.php
@@ -2,5 +2,6 @@
 
 session_start();
 require_once('../Settings/settings.php');
-require_once('../Deliveryaddress/deliveryClass.php');
-Delivery::read($connection,$ID);
+require_once('../PaymentMethod/paymentClass.php');
+Payment::read($connection, $_SESSION['ID']);
+echo '<a href="create.php"> Add </a>';
