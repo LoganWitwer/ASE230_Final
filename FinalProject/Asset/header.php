70ac1222204021f64f4d672f3cfaf6661fc80b5c
LoganWitwer <113311699+LoganWitwer@users.noreply.github.com>
2022-12-11 15:36:17 -0500

    Add files via upload

--- FinalProject/Asset/header.php
+++ FinalProject/Asset/header.php
@@ -6,3 +6,23 @@ require_once("../Asset/head.php");
 </header>
 
 <?php
+if(!isset($_SESSION['role'])){
+
+} else {
+    if($_SESSION['role']==1){
+        echo '<ul style="list-style-type: none;">
+        <li style="display: inline;"><button type="button" class="btn btn-primary"><a href="../Restaurant/index.php"> Add Restaurants and Items </a></button></li>
+        <li style="display: inline;"><button type="button" class="btn btn-primary"><a href="../Order/checkout.php"> Place Order </a></button></li>
+        <li style="display: inline;"><button type="button" class="btn btn-primary"><a href="../Auth/signout.php"> Sign out</a></button></li>
+        <li style="display: inline;"><button type="button" class="btn btn-primary"><a href="../Deliveryaddress/create.php"> Add Delivery Address </a></button></li>
+        <li style="display: inline;"><button type="button" class="btn btn-primary"><a href="../PaymentMethod/create.php"> Add Payment Card </a></button></li>
+        </ul>';
+    } else {
+        echo    '<ul>
+        <li style="display: inline;"><button type="button" class="btn btn-primary"><a href="../Order/checkout.php"> Place Order </a></button></li>
+        <li style="display: inline;"><button type="button" class="btn btn-primary"><a href="../Auth/signout.php"> Sign out</a></button></li>
+        <li style="display: inline;"><button type="button" class="btn btn-primary"><a href="../Deliveryaddress/create.php"> Add Delivery Address </a></button></li>
+        <li style="display: inline;"><button type="button" class="btn btn-primary"><a href="../PaymentMethod/create.php"> Add Payment Card </a></button></li>
+        </ul>';
+    }
+}
