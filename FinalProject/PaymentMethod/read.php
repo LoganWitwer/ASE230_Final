7660b7fdd76660261b6afa8a13ca1f547f3c7663
LoganWitwer <113311699+LoganWitwer@users.noreply.github.com>
2022-12-11 15:53:56 -0500

    Add files via upload

--- FinalProject/PaymentMethod/read.php
+++ FinalProject/PaymentMethod/read.php
@@ -4,3 +4,4 @@ session_start();
 require_once('../Settings/settings.php');
 require_once('../PaymentMethod/paymentClass.php');
 Payment::read($connection, $_SESSION['ID']);
+echo '<a href="create.php"> Add </a>';
